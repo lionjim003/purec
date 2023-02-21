1f276d833e94c37575001b4bcf50483637ed8e8c
lionjim <lionjim003@gmail.com>
2023-02-21 12:42:54 +0800

    update code.c

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-@gcc -std=c17 -static -Wall -o a.exe code.c ^
+@gcc -std=c17 -static -Wall %* -o a.exe code.c ^
 -I../vulkansdk/Include -I../glfw/include -I../cglm/include ^
 -L../vulkansdk/Lib -lvulkan-1 -L../glfw/lib-mingw-w64 -lglfw3 -lgdi32 ^ 
--- code.c
+++ code.c
@@ -19,32 +19,70 @@ void initWindow() {
 #	else
 	bool const enableValidationLayers = true;
 #	endif
-char const* const validationLayers[] = {
-	"VK_LAYER_KHRONOS_validation",
+
+char const* const requiredExtensions[] = {
+	VK_EXT_DEBUG_UTILS_EXTENSION_NAME,
 	NULL,
 };
-char const* const* supportValidationLayers(uint32_t* pLayerCount) {
+void printAvailableExtensions() {
+	uint32_t count = 0;
+	vkEnumerateInstanceExtensionProperties(NULL, &count, NULL);
+	VkExtensionProperties extensions[count];
+	vkEnumerateInstanceExtensionProperties(NULL, &count, extensions);
+	
+	printf("Available extensions:\n");
+	for (uint32_t i = 0; i < count; ++i) {
+		printf("%s\n", extensions[i].extensionName);
+	}
+	return;
+}
+uint32_t getRequiredExtensions(char const** pExtensions) {
+	uint32_t count = 0;
+	char const** glfwRequiredExtensions = 
+		glfwGetRequiredInstanceExtensions(&count);
+		
+	uint32_t extensionCount = 0;
+	for (uint32_t i = 0, already = false, ralready = false; true; ++i) {
+		if (i < count) {
+			if (pExtensions) pExtensions[i] = glfwRequiredExtensions[i];
+			++extensionCount;
+		} else already = true;
+		
+		if (enableValidationLayers && requiredExtensions[i] && !ralready) {
+			if (pExtensions) pExtensions[count + i] = requiredExtensions[i];
+			++extensionCount;
+		} else ralready = true;
+		
+		if (already && ralready) 
+			break;
+	}
+	return extensionCount;
+}
+char const* const requiredLayers[] = {
+	"VK_LAYER_KHRONOS_validation",
+	NULL,	
+};
+void printAvailableLayers() {
+	uint32_t count = 0;
+	vkEnumerateInstanceLayerProperties(&count, NULL);
+	VkLayerProperties layers[count];
+	vkEnumerateInstanceLayerProperties(&count, layers);
+	
+	printf("Available layers:\n");
+	for (uint32_t i = 0; i < count; ++i) {
+		printf("%s\n", layers[i].layerName);
+	}
+	return;
+}
+uint32_t getRequiredLayers(char const** pLayers) {
 	uint32_t layerCount = 0;
-	vkEnumerateInstanceLayerProperties(&layerCount, NULL);
-	printf("layer count: %u\n", layerCount);
-	VkLayerProperties layers[layerCount];
-	vkEnumerateInstanceLayerProperties(&layerCount, layers);
-	
-	
-	bool validationLayersAllSupported = false;
-	uint32_t vLayerCount = 0;
-	for (uint32_t i = 0; i < layerCount; ++i) {
-		printf("layer name:\t%s\n", layers[i].layerName);
-		if (strcmp(validationLayers[vLayerCount], layers[i].layerName) == 0) {
-			if (validationLayers[++vLayerCount] == NULL) {
-				validationLayersAllSupported = true;
-				break;
-			}
-			i = ~0;
+	for (uint32_t i = 0; requiredLayers[i]; ++i) {
+		if (enableValidationLayers) {
+			if (pLayers) pLayers[i] = requiredLayers[i];
+			++layerCount;
 		}
 	}
-	*pLayerCount = validationLayersAllSupported? vLayerCount: 0;
-	return validationLayersAllSupported? validationLayers: NULL;
+	return layerCount;
 }
 VkInstance instance;
 void initVulkan() {
@@ -56,15 +94,20 @@ void initVulkan() {
 		.engineVersion 		= VK_MAKE_VERSION(1, 0, 0),
 		.apiVersion 		= VK_API_VERSION_1_0,
 	};
-	uint32_t extensionCount = 0;
-	uint32_t layerCount 	= 0;
-	char const** extensions = glfwGetRequiredInstanceExtensions(&extensionCount);
-	char const*const* layers= supportValidationLayers(&layerCount);
-	if (!enableValidationLayers || layerCount == 0) {
-		printf("not support validation layers.\n");
-		layerCount = 0;
-		layers = NULL;
-	}
+	uint32_t extensionCount = getRequiredExtensions(NULL);
+	char const* extensions[extensionCount];
+	getRequiredExtensions(extensions);
+	// printAvailableExtensions();
+	// for (uint32_t i = 0; i < extensionCount; ++i) {
+		// printf("Extension: %s\n", extensions[i]);
+	// }
+	uint32_t layerCount = getRequiredLayers(NULL);
+	char const* layers[layerCount];
+	getRequiredLayers(layers);
+	// printAvailableLayers();
+	// for (uint32_t i = 0; i < layerCount; ++i) {
+		// printf("Layer: %s\n", layers[i]);
+	// }
 	VkInstanceCreateInfo createInfo = {
 		.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
 		.pApplicationInfo = &appInfo,
@@ -73,8 +116,9 @@ void initVulkan() {
 		.enabledLayerCount = layerCount,
 		.ppEnabledLayerNames = layers,
 	};
-	if (vkCreateInstance(&createInfo, NULL, &instance) != VK_SUCCESS) {
-		printf("failed to create Vulkan instance.\n");
+	VkResult result = vkCreateInstance(&createInfo, NULL, &instance);
+	if (result != VK_SUCCESS) {
+		printf("failed to create Vulkan instance: %d\n", result);
 	}
 }
 void mainLoop() {
