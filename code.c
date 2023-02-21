9c5483f9f94daef321f24935c6f45c69b1aa54b8
lionjim <lionjim003@gmail.com>
2023-02-21 19:58:23 +0800

    update code.c to 'pick physical device'

--- code.c
+++ code.c
@@ -3,27 +3,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include <string.h>
 
 #define WINDOW_WIDTH  800
 #define WINDOW_HEIGHT 600
 GLFWwindow* window;
+VkInstance instance;
+VkDebugUtilsMessengerEXT debugMessenger;
+VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+
+#ifdef NDEBUG
+	bool const enableValidationLayers = false;
+#else
+	bool const enableValidationLayers = true;
+#endif/* NDEBUG */
+
+char const* const requiredExtensions[] = {
+	VK_EXT_DEBUG_UTILS_EXTENSION_NAME,
+	NULL,
+};
+char const* const requiredLayers[] = {
+	"VK_LAYER_KHRONOS_validation",
+	NULL,	
+};
+
 void initWindow() {
 	glfwInit();
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 	window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Vulkan", NULL, NULL);
 }
-#ifdef NDEBUG
-	bool const enableValidationLayers = false;
-#	else
-	bool const enableValidationLayers = true;
-#	endif
-
-char const* const requiredExtensions[] = {
-	VK_EXT_DEBUG_UTILS_EXTENSION_NAME,
-	NULL,
-};
 void printAvailableExtensions() {
 	uint32_t count = 0;
 	vkEnumerateInstanceExtensionProperties(NULL, &count, NULL);
@@ -34,7 +42,6 @@ void printAvailableExtensions() {
 	for (uint32_t i = 0; i < count; ++i) {
 		printf("%s\n", extensions[i].extensionName);
 	}
-	return;
 }
 uint32_t getRequiredExtensions(char const** pExtensions) {
 	uint32_t count = 0;
@@ -58,10 +65,6 @@ uint32_t getRequiredExtensions(char const** pExtensions) {
 	}
 	return extensionCount;
 }
-char const* const requiredLayers[] = {
-	"VK_LAYER_KHRONOS_validation",
-	NULL,	
-};
 void printAvailableLayers() {
 	uint32_t count = 0;
 	vkEnumerateInstanceLayerProperties(&count, NULL);
@@ -72,7 +75,6 @@ void printAvailableLayers() {
 	for (uint32_t i = 0; i < count; ++i) {
 		printf("%s\n", layers[i].layerName);
 	}
-	return;
 }
 uint32_t getRequiredLayers(char const** pLayers) {
 	uint32_t layerCount = 0;
@@ -84,7 +86,54 @@ uint32_t getRequiredLayers(char const** pLayers) {
 	}
 	return layerCount;
 }
-VkInstance instance;
+VkResult createDebugUtilsMessengerEXT(VkInstance instance, 
+	const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+	const VkAllocationCallbacks* pAllocator,
+	VkDebugUtilsMessengerEXT* pDebugMessenger) {
+	
+	 PFN_vkCreateDebugUtilsMessengerEXT func =
+	(PFN_vkCreateDebugUtilsMessengerEXT)
+		vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+		
+	return (func) ? 
+		func(instance, pCreateInfo, pAllocator, pDebugMessenger) :
+		VK_ERROR_EXTENSION_NOT_PRESENT;
+}
+void destroyDebugUtilsMessengerEXT(VkInstance instance,
+	VkDebugUtilsMessengerEXT debugMessenger,
+	const VkAllocationCallbacks* pAllocator) {
+	
+	 PFN_vkDestroyDebugUtilsMessengerEXT func = 
+	(PFN_vkDestroyDebugUtilsMessengerEXT)
+		vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+		
+	if (func) func(instance, debugMessenger, pAllocator);
+}
+VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+	VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+	VkDebugUtilsMessageTypeFlagsEXT messageType,
+	const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+	void* pUserData) {
+		
+	if (messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT)
+		printf("%s\n", pCallbackData->pMessage);
+	return VK_FALSE;
+}
+void fillDebugUtilsMessengerCreateInfoEXT( 
+	VkDebugUtilsMessengerCreateInfoEXT* createInfo) {
+
+	createInfo->sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+	createInfo->messageSeverity =  
+		VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+	createInfo->messageType = 
+		VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+	createInfo->pfnUserCallback = debugCallback;
+	createInfo->pUserData = NULL;
+}
 void initVulkan() {
 	VkApplicationInfo appInfo = {
 		.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
@@ -108,6 +157,9 @@ void initVulkan() {
 	// for (uint32_t i = 0; i < layerCount; ++i) {
 		// printf("Layer: %s\n", layers[i]);
 	// }
+	VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo;
+	fillDebugUtilsMessengerCreateInfoEXT(&debugCreateInfo);
+	
 	VkInstanceCreateInfo createInfo = {
 		.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
 		.pApplicationInfo = &appInfo,
@@ -115,10 +167,48 @@ void initVulkan() {
 		.ppEnabledExtensionNames = extensions,
 		.enabledLayerCount = layerCount,
 		.ppEnabledLayerNames = layers,
+		.pNext = &debugCreateInfo,
 	};
+	if (!enableValidationLayers)
+		createInfo.pNext = NULL;
+	
 	VkResult result = vkCreateInstance(&createInfo, NULL, &instance);
-	if (result != VK_SUCCESS) {
+	if (result != VK_SUCCESS) 
 		printf("failed to create Vulkan instance: %d\n", result);
+}
+void setupDebugMessenger() {
+	if (!enableValidationLayers) return;	
+	
+	VkDebugUtilsMessengerCreateInfoEXT createInfo;
+	fillDebugUtilsMessengerCreateInfoEXT (&createInfo);
+	if (createDebugUtilsMessengerEXT(instance, &createInfo, NULL, &debugMessenger)
+		!= VK_SUCCESS) printf("failed to set up debug messenger\n");
+}
+bool isDeviceSuitable(VkPhysicalDevice device) {
+	// VkPhysicalDeviceProperties deviceProperties;
+	// vkGetPhysicalDeviceProperties(device, &deviceProperties);
+	// VkPhysicalDeviceFeatures deviceFeatures;
+	// vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+	return true;
+}
+void pickPhysicalDevice() {
+	uint32_t deviceCount = 0;
+	vkEnumeratePhysicalDevices(instance, &deviceCount, NULL);
+	if (deviceCount == 0) {
+		printf("failed to find GPUs with Vulkan support!");
+		return;
+	}
+	VkPhysicalDevice devices[deviceCount];
+	vkEnumeratePhysicalDevices(instance, &deviceCount, devices);
+	for (uint32_t i = 0; i < deviceCount; ++i) {
+		if (isDeviceSuitable(devices[i])) {
+			physicalDevice = devices[i];
+			break;
+		}
+	}
+	if (physicalDevice == VK_NULL_HANDLE) {
+		printf("failed to find a suitable GPU!");
+		return;
 	}
 }
 void mainLoop() {
@@ -126,15 +216,18 @@ void mainLoop() {
 		glfwPollEvents();
 	}
 }
-void cleanup() {	
+void cleanup() {
+	if (enableValidationLayers)
+		destroyDebugUtilsMessengerEXT(instance, debugMessenger, NULL);
 	vkDestroyInstance(instance, NULL);
 	glfwDestroyWindow(window);
 	glfwTerminate();
 }
-
 int main(int argc, char const* argv[]) {
 	initWindow();
 	initVulkan();
+	setupDebugMessenger();
+	pickPhysicalDevice();
 	mainLoop();
 	cleanup();
 	return EXIT_SUCCESS;
